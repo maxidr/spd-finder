d6c02692f23c7240e7bcbd307f1ead2c1bb37986
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-01-25 11:02:37 -0300

    Se genera lo básico para la app web

---
+++ app.rb
@@ -0,0 +1,18 @@
+$:.unshift File.dirname(__FILE__)
+
+#require File.expand_path("settings", File.dirname(__FILE__))
+
+require "cuba"
+
+Cuba.use Rack::MethodOverride
+Cuba.use Rack::Session::Cookie, :secret => SecureRandom.hex(64)
+Cuba.use Rack::Static,
+    root: 'public',
+    urls: ['/js', '/css', '/img']
+
+Cuba.define do
+  on root do 
+    res.write 'hello'
+  end
+end
+
