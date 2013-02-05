795785a7247269dcc8759e7930f071a792a68dc2
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-02-05 09:35:01 -0300

    Implemented redmine database interaction

---
+++ models/redmine_database.rb
@@ -0,0 +1,105 @@
+require 'yaml'
+
+class RedmineDatabase
+
+  attr_reader :db
+
+  def initialize(connection_info = nil)
+    @db = Sequel.connect(connection_info)
+   # Sequel.postgres('database_name', :user=>'user', 
+   #                       :password=>'password', :host=>'host', :port=>5432, 
+   #                              :max_connections=>10)
+  end
+
+  # Return an array with any element represent one project
+  # Project content example:
+  #     
+  #     { id: 1, data: { name: 'project 1', identifier: 'proj_1' } }
+  #
+  # Can use a block parameter that will be invoked on any row iteration.
+  # The block must expected a row as parameter and the row contains:
+  #   + id
+  #   + name
+  #   + identifier
+  #
+  #  Example:
+  #
+  #     all_projects do |row|
+  #       puts row[:id]
+  #       puts row[:name]
+  #       puts row[:identifier]
+  #     end
+  #
+  #
+  # @return [Array<Hash>]
+  #
+  def all_projects(&block)
+    @db.from(:projects).map do |row|
+      if block
+        block.call(row)
+      else
+        { id: row[:id], data: { name: row[:name], identifier: row[:identifier] } }
+      end
+    end
+  end
+
+  # Return an array with any value for the axis identified by the id parameter
+  # Any element of the array contains:
+  #   + axis_id
+  #   + desc [description]
+  #
+  # The values are obtained from the table custom_fields for the custom_field_id
+  # 
+  # Can use a block as parameter to add funcionality in any row iteration.
+  # Example:
+  #
+  #     all_axis(2) do |row|
+  #       puts row[:axis_id]
+  #       puts row[:desc]
+  #     end
+  #
+  # @return [Array<Hash>]
+  #
+  def all_axis(custom_field_id, &block)
+    yaml_values = @db.select(:possible_values).from(:custom_fields).where(id: custom_field_id).first
+    values = YAML.load(yaml_values)
+    values.map do |desc|
+      axis = parse_axis_description(desc)
+      block ? block.call(axis) : axis
+    end
+  end
+
+  # Return the relations between projects and axis. 
+  # The parameter limit the ids of the axis to find
+  #
+  # Can use a block as parameter to process in any row iteration.
+  # Example:
+  #
+  #     projects_axis_relations(1,2) do |row|
+  #       puts row[:project_id]
+  #       puts row[:axis_id]
+  #     end
+  #
+  # @return [Array<Hash>] where any element is a hash with projects_id and axis_id (example: { project_id: 1, axis_id: 10 })
+  #
+  def projects_axis_relations(*custom_fields_ids, &row_processor)
+    @db.select(:customized_id, :value)
+      .from(:custom_values)
+      .where("customized_type = 'Project' and custom_field_id IN ? and value != ''", custom_fields_ids)
+      .map do |val|
+        row = { project_id: val[:customized_id], axis_id: parse_axis_description(val[:value]).fetch(:axis_id) }
+        row_processor ? row_processor.call(row) : row
+      end
+  end
+
+  private
+
+  # Parse the string "010 GestiÃ³n de la informaciÃ³n | AdministraciÃ³n de registros"
+  # into { id: 10, desc: "GestiÃ³n de la informaciÃ³n | AdministraciÃ³n de registros" }
+  # @return [Hash] with id and desc as keys
+  def parse_axis_description(axis_desc)
+    matching = axis_desc.match(/(\d+\s)([\w*\W*\s*]*)/) 
+    { axis_id: matching[1].to_i, desc: matching[2] }
+  end
+  
+end
---
+++ test/redmine_database.rb
@@ -0,0 +1,69 @@
+# encoding: UTF-8
+require File.expand_path("helper", File.dirname(__FILE__))
+require 'sequel/adapters/mock'
+require 'yaml'
+
+scope do
+  prepare do
+    any_instance_of(Sequel::Mock::Database) do |db|
+      stub(db).from(:projects) do
+        [ 
+          { id:1, name: 'test 1', identifier: 'test_1'  },
+          { id:2, name: 'test 2', identifier: 'test_2' },
+        ]
+      end
+      stub(db).select.stub!.from.stub!.where.stub!.first do
+        [
+          '010 GestiÃ³n de la informaciÃ³n | AdministraciÃ³n de registros',
+          '011 GestiÃ³n de la informaciÃ³n | GestiÃ³n documental'
+        ].to_yaml
+      end
+
+      stub(db).select(:customized_id, :value).stub!.from.stub!.where do
+        [ { customized_id: 1, value: "0010 Tipo de proyecto | subtipo de proyecto" } ]
+      end
+    end
+
+    @redmine = RedmineDatabase.new("mock://mysql2")
+  end
+
+  test 'all_axis' do
+    expect = [
+      { axis_id: 10, desc: 'GestiÃ³n de la informaciÃ³n | AdministraciÃ³n de registros' },
+      { axis_id: 11, desc: 'GestiÃ³n de la informaciÃ³n | GestiÃ³n documental' }
+    ]
+    assert_equal expect, @redmine.all_axis(2)
+  end
+
+  test 'all_axis with block as parameter' do
+    @redmine.all_axis(2) do |axis|
+      assert_equal({ axis_id: axis[:axis_id], desc: axis[:desc] }, axis)
+    end
+  end
+
+  test 'all_project' do
+    expect = [ 
+      { id: 1, data: { name: 'test 1', identifier: 'test_1' } },
+      { id: 2, data: { name: 'test 2', identifier: 'test_2' } }
+    ]
+
+    assert_equal expect, @redmine.all_projects
+  end
+
+  test 'all_proyect with block as parameter' do
+    @redmine.all_projects do |row|
+      assert_equal({ id: row[:id], name: row[:name], identifier: row[:identifier] }, row)
+    end
+  end
+
+  test 'projects_axis_relations' do
+    expect = [ { project_id: 1, axis_id: 10 } ]
+    assert_equal expect, @redmine.projects_axis_relations(2,3)
+  end
+
+  test 'projects_axis_relations with block as parameter' do
+    @redmine.projects_axis_relations(2, 3) do |row|
+      assert_equal({ project_id: row[:project_id], axis_id: row[:axis_id] }, row)
+    end
+  end
+end
