ab48aeba0772185fc29b9a27724a25133ced8a5d
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-02-09 11:59:59 -0300

    Otra mejora en el view_helper

--- lib/view_helper.rb
+++ lib/view_helper.rb
@@ -4,7 +4,7 @@ module ViewHelper
     if req.path.end_with?("/")
       req.path + for_path[/\A\/?(.*)/, 1] # Extract / from the begining of for_path
     else
-      req.path + for_path
+      req.path + ( for_path.start_with?("/") ? for_path : "/" + for_path
     end
   end
 end
