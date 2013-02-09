e01e6cbe278541da94734fd622d7ca71e8b47a36
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-02-09 11:57:34 -0300

    Se corrige un error en el view_helper

--- lib/view_helper.rb
+++ lib/view_helper.rb
@@ -1,5 +1,10 @@
 module ViewHelper
   def url(for_path)
-    req.path + for_path[/\A\/?(.*)/, 1] # Extract / from the begining of for_path
+    #req.path + for_path[/\A\/?(.*)/, 1] # Extract / from the begining of for_path
+    if req.path.end_with?("/")
+      req.path + for_path[/\A\/?(.*)/, 1] # Extract / from the begining of for_path
+    else
+      req.path + for_path
+    end
   end
 end
